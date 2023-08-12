938327280201567bcab993fa229681d4cdb50923
jihyuk <kwlgur0416@gmail.com>
2023-08-12 16:38:04 +0900

    LCS 1, 2

---
+++ 10000/9252.py
@@ -0,0 +1,29 @@
+A = input()
+B = input()
+
+a, b = len(A), len(B)
+dp = [[0 for _ in range(b+1)] for _ in range(a+1)]
+
+for i in range(1, a+1):
+    for j in range(1, b+1):
+        if A[i-1] == B[j-1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+size = dp[a][b]
+print(size)
+
+
+LCS = []
+while size != 0:
+    if dp[a-1][b] == size:
+        a = a-1
+    elif dp[a][b-1] == size:
+        b = b-1
+    else:
+        LCS.append(A[a-1])
+        a, b = a-1, b-1
+        size = dp[a][b]
+
+print("".join(reversed(LCS)))
