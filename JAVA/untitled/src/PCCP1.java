5fef20197a9034de9a2817192c01e3d9463e4e67
jihyuk <kwlgur0416@gmail.com>
2023-11-23 23:19:08 +0900

    JAVA PCCP1

---
+++ JAVA/untitled/src/PCCP1.java
@@ -0,0 +1,21 @@
+class PCCP1 {
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int maxHealth = health;
+
+        int attackTime = 0;
+        for (int[] attack : attacks) {
+            int interval = attack[0] - attackTime - 1;
+            int plus = (interval / bandage[0]) * bandage[2] + interval * bandage[1];
+            health = Math.min(health + plus, maxHealth);
+
+            health -= attack[1];
+            if(health <= 0) {
+                return -1;
+            }
+            attackTime = attack[0];
+        }
+
+        return health;
+    }
+
+}
