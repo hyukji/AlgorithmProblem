6c49106c6c479a4ebee56d31f47b4bae5b061b4c
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-01-28 15:44:32 +0900

    [level 2] Title: 소수 찾기, Time: 12.06 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,35 +1,69 @@
-import java.util.HashSet;
+import java.util.*;
+
 class Solution {
+
+    static int maxValue = 0;
+
+    static int n;
+    static int[] visited;
+    static Set<Integer> result = new HashSet<>();
+
     public int solution(String numbers) {
-        HashSet<Integer> set = new HashSet<>();
-        permutation("", numbers, set);
-        int count = 0;
-        while(set.iterator().hasNext()){
-            int a = set.iterator().next();
-            set.remove(a);
-            if(a==2) count++;
-            if(a%2!=0 && isPrime(a)){
-                count++;
-            }
-        }        
-        return count;
-    }
-
-    public boolean isPrime(int n){
-        if(n==0 || n==1) return false;
-        for(int i=3; i<=(int)Math.sqrt(n); i+=2){
-            if(n%i==0) return false;
+        n = numbers.length();
+        
+        visited = new int[n];
+        for(int size=1; size<=n; size++) {
+            permutation("", numbers, size);
         }
-        return true;
+        
+        int[] primes = getPrimes();
+        return countPrime(primes);
     }
 
-        public void permutation(String prefix, String str, HashSet<Integer> set) {
-        int n = str.length();
-        //if (n == 0) System.out.println(prefix);
-        if(!prefix.equals("")) set.add(Integer.valueOf(prefix));
-        for (int i = 0; i < n; i++)
-          permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n), set);
+    private void permutation(String out, String nums, int size) {
+        if(out.length() == size) {
+        	int value = Integer.parseInt(out);
+        	if(maxValue < value) maxValue = value;
+            result.add(value);
+            return;
+        }
+
+        for(int i = 0 ; i < n; i++) {
+            if(visited[i] == 1) continue;
+            visited[i] = 1;
+            permutation(out + nums.charAt(i), nums, size);
+            visited[i] = 0;
+        }
 
     }
+    
+    private int[] getPrimes() {
+        int[] primes = new int[maxValue+1];
 
+        primes[0] = 1;
+        primes[1] = 1;
+        for(int i = 2; i <= Math.sqrt(maxValue); i++) {
+            if(primes[i] == 0) checkNotPrime(primes, i);
+        }
+        
+        return primes;
+    }
+
+    private void checkNotPrime(int[] primes, int i) {
+        int num = i + i;
+        while(num <= maxValue) {
+            primes[num] = 1;
+            num+=i;
+        }
+    }
+    
+    private int countPrime(int[] primes) {
+        int answer = 0;
+        for(int v :result) {
+            if(primes[v] == 0) answer++;
+        }
+
+        return answer;
+    }
+    
 }
