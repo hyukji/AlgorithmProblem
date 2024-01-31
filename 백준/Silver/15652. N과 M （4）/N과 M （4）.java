b598e1833634a708e98f24256c9a98283ce6c425
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-01-31 15:45:52 +0900

    [Silver III] Title: N과 M (4), Time: 164 ms, Memory: 17244 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,33 +1,41 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int M;
-    static int N;
-    static int[] arr;
-    static StringBuilder sb = new StringBuilder();
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr=new int[M];
-        dfs(0,0);
-        System.out.print(sb);
-    }
-    static void dfs(int at, int depth){
-        if(M==depth){
-            for(int val : arr){
-                sb.append(val).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
-        for(int i=at; i<N; i++){
-            arr[depth]=i+1;
-            dfs(i,depth+1);
-        }
-    }
+	
+	static int n, m;
+	static int[] result;
+	static StringBuilder sb  =  new StringBuilder();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		result  = new int[m];
+		
+		comb(0, 1);
+		
+		System.out.print(sb);
+	}
+
+	private static void comb(int cnt, int start) {
+		if(cnt == m) {
+			for(int v : result) sb.append(v).append(" ");
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = start; i <= n; i++) {
+			result[cnt] = i;
+			comb(cnt+1, i);
+		}
+		
+	}
+	
+	
+	
 }
