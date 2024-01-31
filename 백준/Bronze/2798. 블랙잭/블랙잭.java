8939c955cff62f48c84c9d613e0cdb812213db05
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-01-31 17:39:51 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,49 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n, m;
-	static int[] graph;
-	static int answer;
-	static int ansDiff = Integer.MAX_VALUE;
-	static int result = 0;
-	
-	public static void main(String[] args) throws Exception {
+	static int N, M;
+	static int[] nums;
+	static int diff = Integer.MAX_VALUE;
+	static int ans = -1;
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		graph = new int[n];
-
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i =0; i < n; i++) {
-			graph[i] = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		nums = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		comb(0, 0);
+		comb(0, 0, 0, 3);
 		
-		System.out.print(answer);
+		System.out.println(ans);
 	}
-
-	private static void comb(int cnt, int start) {
-		if(cnt == 3) {
-			int diff =m - result;
-			if(diff >= 0 && diff < ansDiff) {
-				ansDiff =diff;
-				answer = result;
+	
+	static void comb(int s, int cnt, int sum, int R) {
+		if(diff == 0 || sum > M) {
+			return;
+		}
+		if(cnt == R) {
+			if(diff > Math.abs(M - sum)) {
+				diff = Math.min(diff, Math.abs(M - sum));
+				ans = sum;
 			}
 			return;
 		}
 		
-		for(int i = start; i < n; i++) {
-			result += graph[i];
-			comb(cnt+1, i+1);
-			result -=  graph[i];
+		for(int i = s; i < N; i++) {
+			comb(i + 1, cnt + 1, sum + nums[i], R);
 		}
 	}
-	
 }
