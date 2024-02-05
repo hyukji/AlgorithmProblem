0e26588021a5162c992bc19983b1077057798048
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-02-05 16:54:38 +0900

    [Silver I] Title: 안전 영역, Time: 200 ms, Memory: 18780 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int maxH = Integer.MIN_VALUE, minH = Integer.MAX_VALUE, n;
+	static int[][] graph, visited;
+
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		n = Integer.parseInt(br.readLine());
+		graph = new int[n][n];
+		for(int r =0; r < n; r++) { 
+			st = new StringTokenizer(br.readLine());
+			for(int c =0; c < n; c++) {
+				int v= Integer.parseInt(st.nextToken());
+				graph[r][c] = v;
+				if(maxH < v) maxH = v;
+				if(minH > v) minH = v;
+			}
+		}
+		
+		int answer = 1;
+		for(int rain = minH; rain < maxH; rain++) {
+			visited = new int[n][n];
+			int cnt = 0;
+			for(int r =0; r < n; r++) { 
+				for(int c =0; c < n; c++) {
+					if(visited[r][c] == 1) continue;
+					visited[r][c] = 1;
+					if(graph[r][c] > rain) {
+						cnt++;
+						dfs(r, c, rain);
+//						System.out.println("rain " + rain + ",cnt "+ cnt + " " + r + " " + c);
+					}
+				}
+			}
+			if(answer < cnt) answer = cnt;
+		}
+		
+		System.out.print (answer);
+	}
+
+	private static void dfs(int r, int c, int rain) {
+		for(int d = 0; d < 4; d++) {
+			int nr = r + dr[d]; 
+			int nc = c + dc[d]; 
+			if(nr <0 || nr >= n || nc < 0 || nc >= n) continue;
+			if(visited[nr][nc] == 1) continue;
+			visited[nr][nc] = 1;
+			if(graph[nr][nc] > rain) dfs(nr, nc, rain);
+		}
+	}
+
+}
