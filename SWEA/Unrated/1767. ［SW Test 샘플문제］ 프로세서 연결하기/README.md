e52007b36da77a0b0555d6db569a1fe93f66aefd
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-02-10 23:49:55 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 328 ms, Memory: 88,872 KB -BaekjoonHub

---
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [SW Test 샘플문제] 프로세서 연결하기 - 1767 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV4suNtaXFEDFAUf) 
+
+### 성능 요약
+
+메모리: 88,872 KB, 시간: 328 ms, 코드길이: 2,369 Bytes
+
+### 제출 일자
+
+2024-02-10 23:49
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution  {
+	
+	static final int[] dr = {1, 0, -1, 0};
+	static final int[] dc = {0, 1, 0, -1};
+	
+	static int n, totCnt, maxConnected, maxLines;
+	static int[][] cores;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc < T+1; tc++) {
+			cores = new int[12][2];
+			maxConnected = 0;
+			maxLines = 0;
+			totCnt = 0;
+			
+			n = Integer.parseInt(br.readLine());
+			
+			int[][] graph = new int[n][n];
+			for(int  r =0; r <n; r++) {
+				st = new StringTokenizer(br.readLine());
+				for(int  c =0; c <n; c++) {
+					graph[r][c] = Integer.parseInt(st.nextToken());
+					if(graph[r][c] == 1) cores[totCnt++] = new int[] {r, c};
+				}
+			}
+
+			dfs(0, 0, 0, graph);
+			
+			sb.append("#").append(tc).append(" ").append(maxLines).append(" ").append("\n");
+		}
+		
+		System.out.print(sb);
+	}
+
+	private static void dfs(int cnt, int connected, int lines, int[][] graph) {
+		if(totCnt - cnt + 1 < maxConnected - connected) return;
+		
+		if(cnt == totCnt) {
+			if(connected > maxConnected || (connected == maxConnected && lines < maxLines)) {
+				maxConnected = connected;
+				maxLines = lines;
+			}
+			return;
+		}
+
+		int r = cores[cnt][0];
+		int c = cores[cnt][1];
+		if(r == 0 || c == 0 || r == n-1 || c == n-1) {
+			dfs(cnt+1, connected+1, lines, graph);
+			return;
+		}
+		
+		for(int d = 0; d < 4; d++) {
+			if(check(r, c, d, graph)) {
+				int[][] nGraph = copy(graph);
+				int plusLines = 0;
+				int nr = r;
+				int nc = c;
+				
+				while(true) {
+					nr += dr[d]; nc += dc[d];
+					if(nr < 0 || nr >= n || nc < 0 || nc >= n) break;
+					nGraph[nr][nc] = 2;
+					plusLines++;
+				}
+				
+				dfs(cnt+1, connected+1, lines + plusLines, nGraph);
+			}
+		}
+
+		dfs(cnt+1, connected, lines, graph);
+	}
+
+	private static int[][] copy(int[][] graph) {
+		int[][] nGraph = new int[n][n];
+		for(int r = 0; r <n; r++) {
+			for(int c = 0; c <n; c++) {
+				nGraph[r][c] = graph[r][c];
+			}
+		}
+		return nGraph;
+	}
+
+	private static boolean check(int r, int c, int d, int[][] graph) {
+		while(true) {
+			r += dr[d]; c += dc[d];
+			if(r < 0 || r >= n || c < 0 || c >= n) return true;
+			if(graph[r][c] != 0) return false;
+		}
+	}
+	
+
+}
