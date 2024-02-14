2536cc708ba42a78aaaababfad7b6b97369bab53
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-02-14 10:33:36 +0900

    [Silver I] Title: 쿼드트리, Time: 92 ms, Memory: 12836 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -6,13 +6,11 @@ public class Main {
 	static int n;
 	static char[][] graph;
 	
-	static int[] dr = {0, 0, 1, 1};
-	static int[] dc = {0, 1, 0, 1};
+	static final int[] dr = {0, 0, 1, 1};
+	static final int[] dc = {0, 1, 0, 1};
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = null;
 		
 		n = Integer.parseInt(br.readLine());
 		graph = new char[n][n];
@@ -26,23 +24,30 @@ public class Main {
 		System.out.println(quadTree(0, 0, n));
 	}
 
+	/*
+	 * r, c : quadTree 압축의 시작 좌표
+	 * m : quadTree 압축을 위한 한 변의 길이
+	 */
 	private static String quadTree(int r, int c, int m) {
-		if(m == 1)  return "" + graph[r][c];
+		if(m == 1)  return Character.toString(graph[r][c]); // 압축할 것이 없음 -> 그대로 반환
 
-		String result = "";
+		StringBuilder result = new StringBuilder();
+		result.append("(");
+		int half = m/2;
 		for(int i = 0; i < 4; i++) {
-			result += quadTree(r + dr[i] * m/2, c + dc[i] * m/2, m/2);
+			int nr = r + dr[i] * half;
+			int nc = c + dc[i] * half;
+			result.append(quadTree(nr, nc, half)); // 4개로 쪼개서 해결 -> result에는 4개로 쪼갠 각각의 결과가 저장됨
 		}
-
-		if(result.length() == 4) {
-			for(int i = 0; i < 3; i++) {
-				if(result.charAt(i) != result.charAt(i+1)) return "(" + result + ")";
-			}
-			 return "" + result.charAt(0);
+		result.append(")");
+		
+		 // 압축이 가능하려면 각각이 4가지의 결과가 0 or 1이어야 하며, 모두 같아야 함.
+		if(result.length() != 6) return result.toString(); // 괄호를 고려하면 result의 크기가 6
+		for(int i = 1; i < 4; i++) {
+			if(result.charAt(i) != result.charAt(i+1)) return result.toString();  // 쪼갠 결과가 같지 않다면
 		}
 		
-		return "(" + result + ")";
-		
+		 return Character.toString(result.charAt(1)); // 압축 성공!
 	}
 
 }
