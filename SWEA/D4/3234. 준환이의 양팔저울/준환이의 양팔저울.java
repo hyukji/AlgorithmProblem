b0a6d3363615f9d100f813a8e939fccb666ad529
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-02-21 17:18:48 +0900

    [D4] Title: 준환이의 양팔저울, Time: 117 ms, Memory: 24,844 KB -BaekjoonHub

--- SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Solution  {
 
 	static int[] graph ;
-	static boolean[] visited;
-	static int n, tot, answer;
+	static int n, tot;
+	static int[][] dp;
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +14,6 @@ public class Solution  {
 
 		int T = Integer.parseInt(br.readLine());
 		for(int tc = 1; tc < T+1; tc++) {
-			answer = 0;
 			tot = 0;
 			n = Integer.parseInt(br.readLine());
 			
@@ -22,16 +21,11 @@ public class Solution  {
 			st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < n; i++) {
 				graph[i] = Integer.parseInt(st.nextToken());
-				tot+= graph[i];
+				tot+= graph[i]; // 총 합 더해주기
 			}
-			Arrays.sort(graph);
 
-			for(int i = 0; i < n; i++) {
-				visited = new boolean[n];
-				visited[i] = true;
-				dfs(graph[i], 0, 1);
-			}
-			
+			dp = new int[1<<n][1<<n];
+			int answer = dfs(0, 0, 0, 0, 0);
 			sb.append("#").append(tc).append(" ").append(answer).append("\n");
 		}
 		
@@ -39,28 +33,45 @@ public class Solution  {
 
 	}
 
-	private static void dfs(int left, int right, int cnt) {
-		if(cnt == n) {
-			answer++;
-			return;
+	/*
+	 * left, right 는 왼쪽 오른쪽 각각의 무게의 합
+	 * lv는 왼쪽 무게추의 방문 bit 마스킹
+	 * rv는 오른쪽 무게추의 방문 bit 마스킹
+	 * cnt 확인한 무게추의 개수
+	 * */
+	private static int dfs(int left, int right, int lv, int rv, int cnt) {
+		int result = 0;
+		int v = lv | rv; // 확인 총 무게추의 비트 마스킹
+		
+		if(cnt == n) { // 모두 확인했다면 1
+			dp[lv][rv] = 1;
+			return 1;
 		}
 		
-		if(left > tot/2) {
+		if(left > tot/2) { // 왼쪽 무게 합이 총합의 절반 이상이면, 이후에는 무슨짓을 해도 상관없음. 경우의 수 계산해서 더함
 			int fac = 1;
 			for(int i = 1 ; i <= n-cnt; i++) fac *=i;
-			answer+= fac * Math.pow(2, (n-cnt));
+			result = (int) (fac * Math.pow(2, (n-cnt)));
+			dp[lv][rv] = result; // 현재 상황 dp에 저장
 			
-			return;
+			return result;
 		}
 		
 		for(int i = 0; i < n; i++) {
-			if(visited[i]) continue;
-			visited[i] = true;
-			int w = graph[i];
-			dfs(left + w, right, cnt+1);
-			if(left >= right + w) dfs(left, right + w, cnt+1);
-			visited[i] = false;
+			if((v & (1<< i)) > 0) continue; // i번째 무게추를 이전에 방문했다면 pass
+			
+			int nlv = lv | (1<<i); // 왼쪽에 무게추 넣은 경우
+			if(dp[nlv][rv] > 0)  result+= dp[nlv][rv]; // 이전에 넣은 적이 있다면 
+			else result += dfs(left + graph[i], right, nlv, rv, cnt+1); // 없다면 계싼
+			
+			if(left < right + graph[i]) continue; 
+			int nrv = rv | (1<<i); // 오른쪽에 무게추 넣은 경우
+			if(dp[lv][nrv] > 0)  result += dp[lv][nrv];
+			else result+=dfs(left, right + graph[i], lv, nrv, cnt + 1);
 		}
+		
+		dp[lv][rv] = result; // 현재 상황을 dp에 저장
+		return result;
 	}
 
 }
