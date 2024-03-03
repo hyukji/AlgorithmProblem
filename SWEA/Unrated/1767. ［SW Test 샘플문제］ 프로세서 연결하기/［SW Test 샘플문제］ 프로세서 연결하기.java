59f298ea873ca241013d8deb9c3fc325564cc523
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-03-03 13:34:40 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 197 ms, Memory: 24,064 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -6,8 +6,9 @@ public class Solution  {
 	static final int[] dr = {1, 0, -1, 0};
 	static final int[] dc = {0, 1, 0, -1};
 	
-	static int n, totCnt, maxConnected, maxLines;
+	static int n, coreIdx, maxCore, minLine;
 	static int[][] graph, cores;
+	static int[][] lines;
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,89 +17,83 @@ public class Solution  {
 		
 		int T = Integer.parseInt(br.readLine());
 		for(int tc = 1; tc < T+1; tc++) {
-			cores = new int[12][2];
-			maxConnected = 0;
-			maxLines = 0;
-			totCnt = 0;
+			cores = new int[13][2];
+			
+			coreIdx = 1;
+			maxCore = 0;
+			minLine = Integer.MAX_VALUE;
 			
 			n = Integer.parseInt(br.readLine());
 			graph = new int[n][n];
+			lines = new int[n][n];
 			
+			int coreCnt = 0;
 			for(int  r =0; r <n; r++) {
 				st = new StringTokenizer(br.readLine());
 				for(int  c =0; c <n; c++) {
 					graph[r][c] = Integer.parseInt(st.nextToken());
-					if(graph[r][c] == 1) cores[totCnt++] = new int[] {r, c}; // 프로세서의 코어 따로 저장 
+					if(graph[r][c] == 1) {
+						if(r == 0 || r == n-1 || c == 0 || c == n-1) coreCnt++;
+						else cores[coreIdx++] = new int[] {r, c}; // 프로세서의 코어 따로 저장 
+					}
 				}
 			}
 
-			dfs(0, 0, 0);
+			dfs(1, coreCnt, 0);
 			
-			sb.append("#").append(tc).append(" ").append(maxLines).append(" ").append("\n");
+			sb.append("#").append(tc).append(" ").append(minLine).append(" ").append("\n");
 		}
-		
+	
 		System.out.print(sb);
 	}
 
-	private static void dfs(int cnt, int connected, int lines) {
-		if(totCnt - cnt + 1 < maxConnected - connected) return; // 앞으로 모든 코어를 연결해도 최대가 아닐때 
-		
-		if(cnt == totCnt) { // 모든 코어에 대해 탐색이 끝난 후 
-			if(connected > maxConnected || (connected == maxConnected && lines < maxLines)) {
-				maxConnected = connected;
-				maxLines = lines;
+	private static void dfs(int i, int coreCnt, int lineCnt) {
+		if(i == coreIdx) {
+			if(maxCore < coreCnt || (maxCore == coreCnt && minLine > lineCnt)) {
+				maxCore = coreCnt;
+				minLine = lineCnt;
+//				for(int[] row : lines) System.out.println(Arrays.toString(row));
 			}
 			return;
 		}
 
-		int r = cores[cnt][0];
-		int c = cores[cnt][1];
-		if(r == 0 || c == 0 || r == n-1 || c == n-1) { // 맨 끝라인에 있다면 자동 연결 
-			dfs(cnt+1, connected+1, lines);
-			return;
+		for(int d = 0; d < 4; d++) {
+			int filled = fill(i, d); 
+			if(filled > 0) dfs(i+1, coreCnt+1, lineCnt + filled);
+			rollBack(i, d); 
+		}
+		dfs(i+1, coreCnt, lineCnt);
+	}
+
+	private static void rollBack(int i, int d) {
+		int nr = cores[i][0];
+		int nc = cores[i][1];
+		
+		while(true) {
+			nr += dr[d];
+			nc += dc[d];
+			if(nr < 0 || nr >= n || nc < 0 || nc >= n) break; // 라인 다 채웠으
+			if(lines[nr][nc] == i) lines[nr][nc] = 0;
+			else return;
 		}
 		
-		for(int d = 0; d < 4; d++) { // 연결 가능하다면 연결! 
-			if(check(r, c, d)) {
-				int plusLines = connect(r, c, d);
-				dfs(cnt+1, connected+1, lines + plusLines);
-				rollback(r, c, d);
-			};
-		}
-
-		dfs(cnt+1, connected, lines); // 연결하지 않은 경우 
 	}
 
-
-	private static void rollback(int r, int c, int d) {
+	private static int fill(int i, int d) {
+		int nr = cores[i][0];
+		int nc = cores[i][1];
+		
+		int filled = 0;
 		while(true) {
-			r += dr[d]; c += dc[d];
-			if(r < 0 || r >= n || c < 0 || c >= n) return;
-			if(graph[r][c] != 2) return;
-			
-			graph[r][c] = 0;
+			nr += dr[d];
+			nc += dc[d];
+			if(nr < 0 || nr >= n || nc < 0 || nc >= n) break; // 라인 다 채웠으
+			if(graph[nr][nc] == 1 || lines[nr][nc] != 0) return 0; // 가는 길에 다른 코어나 라인이있다
+			lines[nr][nc] = i;
+			filled++;
 		}
+		
+		return filled;
 	}
-
-	
-	private static boolean check(int r, int c, int d) {
-		while(true) {
-			r += dr[d]; c += dc[d];
-			if(r < 0 || r >= n || c < 0 || c >= n) return true;
-			if(graph[r][c] != 0) return false;
-		}
-	}
-
-	private static int connect(int r, int c, int d) { // 연결 가능한지 확인 후 가능하다면 필요한 선의 개수 
-		int plusLines = 0;
-		while(true) {
-			r += dr[d]; c += dc[d];
-			if(r < 0 || r >= n || c < 0 || c >= n) return plusLines;
-			
-			graph[r][c] = 2;
-			plusLines++;
-		}
-	}
-	
 
 }
