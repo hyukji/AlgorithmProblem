d36afc180e8c06fe5a2c1106e2f71a8bd3d2ff26
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-03-21 11:50:32 +0900

    [Gold I] Title: 부분수열의 합 2, Time: 1160 ms, Memory: 149376 KB -BaekjoonHub

--- Python/백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
+++ Python/백준/Gold/1208. 부분수열의 합 2/부분수열의 합 2.py
@@ -2,28 +2,27 @@ import sys
 input = sys.stdin.readline
 
 from itertools import combinations
-from bisect import bisect_left, bisect_right
+from collections import Counter
 
 def getArr(values):
     nArr = []
     for i in range(1, len(values)+1):
         for comb in combinations(values, i):
             nArr.append(sum(comb))
-    return sorted(nArr)
+    return nArr
 
 n, s = map(int, input().split())
 arr = list(map(int, input().split()))
 
 mid = n//2
-left = getArr(arr[:mid])
-right = getArr(arr[mid:])
+left = Counter(getArr(arr[:mid]))
+right = Counter(getArr(arr[mid:]))
 
 answer = 0
-for l in left:
-    t = s - l
-    answer += bisect_right(right, t) - bisect_left(right, t)
+for l in left.keys():
+    r = s - l
+    answer += left[l] * right[r]
 
-answer += bisect_right(left, s) - bisect_left(left, s)
-answer += bisect_right(right, s) - bisect_left(right, s)
+answer += left[s] + right[s]
 
 print(answer)
