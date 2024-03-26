838731cc22f3134f1326067efa2989d0d0630363
jihyuk <52347271+hyukji@users.noreply.github.com>
2024-03-26 13:10:48 +0900

    [Gold V] Title: 평범한 배낭, Time: 408 ms, Memory: 34608 KB -BaekjoonHub

--- Python/백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ Python/백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -3,6 +3,7 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 data = [list(map(int, input().split())) for _ in range(n)] # w, v를 모아둔다.
+data.sort(reverse=True)
 
 d = {0:0}
 for dw, dv in data:
