7261bd07952a39115304d1738090d1d5102677a5
hyukji <52347271+hyukji@users.noreply.github.com>
2024-04-13 00:17:50 +0900

    [Gold I] Title: 트리의 순회, Time: 284 ms, Memory: 73004 KB -BaekjoonHub

--- 백준/Gold/2263. 트리의 순회/트리의 순회.py
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.py
@@ -1,32 +1,32 @@
 import sys
-sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
-def preorder(inorder_s, inorder_e, postorder_s, postorder_e):
-    # 재귀함수 종료 조건
-    if (inorder_s > inorder_e) or (postorder_s > postorder_e):
+def printPreOrder(in_s, in_e, po_s, po_e):
+    if(in_s == in_e):
+        print(in_order[in_s] , end=" ")
         return
     
-    # 후위 순회의 마지막 노드 출력
-    p = postorder[postorder_e]
-    print(p, end =' ')
+    root = post_order[po_e]
+    pos = position[root]
+
+    left_size = pos - in_s
+    right_size = in_e - pos
     
-    # 후위 순회의 마지막 값이 중위 순회에 어디에 위치 하는지 구했던 리스트가 여기서 사용
-    left = inorder_pos[p] - inorder_s
-    right = inorder_e - inorder_pos[p]
-    
-    # 왼쪽 서브 트리
-    preorder(inorder_s, inorder_s+left-1, postorder_s, postorder_s+left-1)
-    # 오른쪽 서브 트리
-    preorder(inorder_e - right + 1, inorder_e, postorder_e - right, postorder_e - 1)
+    print(root , end=" ")
+
+    if(left_size > 0):
+        printPreOrder(in_s, in_s+left_size-1, po_s, po_s+left_size-1)
+    if(right_size > 0):
+        printPreOrder(in_e-right_size+1, in_e, po_e-right_size, po_e-1)
+            
 
 n = int(input())
-inorder = list(map(int, input().split()))
-postorder = list(map(int, input().split()))
-inorder_pos = [0] * (n+1)
+in_order = list(map(int, input().split())) # left, root, right
+post_order = list(map(int, input().split())) # left, right, root
 
-# 후위순회의 마지막값이 중위순회 몇번째에 존재하는지 알기 위해 사용. 범위를 나누는 기준!
+position = [0 for _ in range(n+1)] # 특정 값이 값이 in_order의 어느 위치에 있는지 **** 매번 찾는 과정은 비효율적임!!! *****
 for i in range(n):
-    inorder_pos[inorder[i]] = i
-    
-preorder(0, n-1, 0, n-1)
+    position[in_order[i]] = i
+
+printPreOrder(0, n-1, 0, n-1) # root, left, right
