0c47360b71b9efc0000f573020be89fc4ed71e94
hyukji <52347271+hyukji@users.noreply.github.com>
2024-05-05 23:47:24 +0900

    [Gold V] Title: 최장 최장 증가 부분 수열, Time: 192 ms, Memory: 21824 KB -BaekjoonHub

--- 백준/Gold/25343. 최장 최장 증가 부분 수열/최장 최장 증가 부분 수열.java
+++ 백준/Gold/25343. 최장 최장 증가 부분 수열/최장 최장 증가 부분 수열.java
@@ -1,59 +1,48 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		
-		int n = Integer.parseInt(br.readLine());
-		int[][] graph = new int[n][n];
-		for(int r = 0; r < n; r++) {
-			st = new StringTokenizer(br.readLine());
-			for(int c = 0; c < n; c++) {
-				graph[r][c] = Integer.parseInt(st.nextToken());
-			}
-		}
+    static final int INF = 10_001;
+    static int N;
+    static int[][] arr;
+    static int[][][] dp;
 
-		int[][] dp = new int[n][n]; // 각각 위치에서의 최대 lis값!
-		// 결국 오른쪽, 아래로만 움직임 -> r,c 의 lis라면 00부터 rc 까지의 사각형에서 
-		// rc의 값보다 작은 값에서의 lis 들 중에 최대+1 이다. 
-		
-		dp[0][0] = 1;
-		for(int c = 1; c < n; c++) {
-			dp[0][c] = 1;
-			for(int nc = 0; nc < c; nc++) {
-				if(graph[0][nc] < graph[0][c]) 
-					dp[0][c] = Integer.max(dp[0][c] ,dp[0][nc] + 1);
-			}
-		}
-		
-		
-		for(int r = 1; r < n; r++) {
-			dp[r][0] = 1;
-			for(int nr = 0; nr < r; nr++) {
-				if(graph[nr][0] < graph[r][0]) 
-					dp[r][0] = Integer.max(dp[r][0], dp[nr][0] + 1);
-			}
-			
-			for(int c = 1; c < n; c++) {
-				dp[r][c] = 1;
-				for(int nr = 0; nr < r+1; nr++) {
-					for(int nc = 0; nc < c+1; nc++) {
-						if(graph[nr][nc] < graph[r][c]) 
-							dp[r][c] = Integer.max(dp[r][c], dp[nr][nc] + 1);
-					}
-				}
-			}
-		}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-		int answer = 0;
-		for(int r = 0; r < n; r++) {
-			for(int c = 0; c < n; c++) {
-				answer = Integer.max(answer, dp[r][c]);
-			}
-		}
-		System.out.println(answer);
-	}
+        dp = new int[201][N + 1][N + 1];
+        for (int i = 1; i <= 200; i++) {
+            for (int j = 0; j <= N; j++) {
+                Arrays.fill(dp[i][j], INF);
+            }
+        }
+
+        for (int r = 1; r <= N; r++) {
+            for (int c = 1; c <= N; c++) {
+                for (int k = 1; k <= 200; k++) {
+                    dp[k][r][c] = Math.min(dp[k][r - 1][c], dp[k][r][c - 1]);
+                    if (Math.min(dp[k - 1][r - 1][c], dp[k - 1][r][c - 1]) < arr[r][c]) {
+                        dp[k][r][c] = Math.min(dp[k][r][c], arr[r][c]);
+                    }
+                }
+            }
+        }
+
+        for (int k = 200; k > 0; k--) {
+            if (dp[k][N][N] != INF) {
+                System.out.println(k);
+                return;
+            }
+        }
+    }
 }
