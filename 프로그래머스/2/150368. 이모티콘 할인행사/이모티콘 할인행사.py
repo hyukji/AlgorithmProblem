c09702d792618a9985d7b87df71ee245929d413e
hyukji <52347271+hyukji@users.noreply.github.com>
2024-06-20 23:46:40 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,37 +1,23 @@
 from itertools import product
 
-def purchase(user, emoticons, sales):
-    money = 0
-    
-    for i, sale in enumerate(sales):
-        if sale >= user[0]:
-            money += emoticons[i] * (100 - sale) / 100
-    
-    if money < user[1]:
-        return False, money
-    
-    return True, 0
-
 def solution(users, emoticons):
-    percent, price = zip(*users)
-    n = len(users)
     m = len(emoticons)
+    cases = product([10, 20, 30, 40], repeat=m)
     
-    maxCount = 0
-    maxMoney = 0
-    for sale in product([10, 20,30, 40], repeat=m):
-        count, money = 0, 0
-        for i in range(n):
-            plusService, userMoney = purchase(users[i], emoticons, sale)
-            if plusService == True:
-                count += 1
+    answer = [0, 0]
+    for case in cases:
+        result = [0, 0]
+        for percent, money in users:
+            cost = 0
+            for i, sale in enumerate(case):
+                if percent <= sale:
+                    cost += (emoticons[i] * (100 - sale) * 0.01)
+            
+            if cost >= money:
+                result[0] += 1
             else:
-                money += userMoney
-        
-        if maxCount < count:
-            maxCount = count
-            maxMoney = money
-        elif maxCount == count and maxMoney < money:
-            maxMoney = money
-        
-    return [maxCount, maxMoney]
+                result[1] += cost
+                
+        answer = max(result, answer)
+    
+    return answer
