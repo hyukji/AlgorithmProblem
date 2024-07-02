4b57ff90cf52dc26df75dc28f84e8eb164b7cd7f
hyukji <52347271+hyukji@users.noreply.github.com>
2024-07-02 17:07:21 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2520 ms, Memory: 440236 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,17 +2,14 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuffer sb = new StringBuffer();
 
         int T = Integer.parseInt(br.readLine());
         for(int tc=0;tc<T;tc++){
-            PriorityQueue<Integer> lpq = new PriorityQueue<>();
-            PriorityQueue<Integer> hpq = new PriorityQueue<>((o1, o2) -> -Integer.compare(o1, o2));
-            Map<Integer, Integer> map = new HashMap<>();
-            int cnt = 0;
+            TreeMap<Integer, Integer> map = new TreeMap<>();
 
             int m = Integer.parseInt(br.readLine());
             for(int i=0;i<m;i++){
@@ -21,40 +18,19 @@ public class Main {
                 int v = Integer.parseInt(st.nextToken());
 
                 if(cmd.equals("I")) {
-                    hpq.offer(v);
-                    lpq.offer(v);
-                    cnt++;
                     map.put(v, map.getOrDefault(v, 0) + 1);
                 } else {
-                    if(cnt == 0) continue;
+                    if(map.isEmpty()) continue;
 
-                    if(v == 1) {
-                        int polled = hpq.poll();
-                        while(map.get(polled) == 0) polled = hpq.poll();
-                        map.put(polled, map.get(polled) - 1);
-                    }
-                    else {
-                        int polled = lpq.poll();
-                        while(map.get(polled) == 0) polled = lpq.poll();
-                        map.put(polled, map.get(polled) - 1);
-                    }
-
-                    if(--cnt == 0) {
-                        hpq.clear();
-                        lpq.clear();
-                    }
+                    int key = (v == 1) ? map.lastKey() : map.firstKey();
+                    int value = map.get(key) - 1;
+                    if(value != 0) map.put(key, value);
+                    else map.remove(key);
                 }
             }
-            if(cnt ==0) sb.append("EMPTY").append("\n");
-            else {
-                int hPolled = hpq.poll();
-                while(map.get(hPolled) == 0) hPolled = hpq.poll();
 
-                int lPolled = lpq.poll();
-                while(map.get(lPolled) == 0) lPolled = lpq.poll();
-
-                sb.append(hPolled).append(" ").append(lPolled).append("\n");
-            }
+            if(map.isEmpty()) sb.append("EMPTY").append("\n");
+            else sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
         }
 
         System.out.println(sb);
