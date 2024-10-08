26d15d84574949b56bc6d567a0c040474d901d40
hyukji <52347271+hyukji@users.noreply.github.com>
2024-10-09 08:29:04 +0900

    [level 2] Title: H-Index, Time: 0.97 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,19 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        
         Arrays.sort(citations);
-        
-        int h = 0;
         int n = citations.length;
-        for(int i = 0; i < n; i++) {
-            int remain = n - i; // 현재보다 같거나 큰 인용수를 가진 논문 수
-            int c = citations[i]; // 인용 수
-            int nh = Math.min(remain, c);
-            h = Math.max(h, nh);
+        int answer = 0;
+        // System.out.println(Arrays.toString(citations));
+        
+        int i = 0;
+        for(int h = 1; h <= 10000; h++) {
+            while(citations[i] < h) {
+                if(++i == n) return answer;
+            }
+            
+            // System.out.println(h + " " + i);
+            int c = citations[i];
+            if(n-i >= h) answer = h;
         }
         
-        return h;
+        return answer;
     }
 }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 80.2 MB, 시간: 0.44 ms
+메모리: 74.7 MB, 시간: 0.97 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 23:25:41
+2024년 10월 09일 08:29:00
 
 ### 문제 설명
 
